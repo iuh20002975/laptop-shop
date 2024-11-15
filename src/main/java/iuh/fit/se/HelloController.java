6512211e0ccfecd768ce75e505d5d91d3f400a79
Nguyen Long <longsky0912624119@gmail.com>
2024-11-15 15:15:34 +0700

    setup enviroment

--- src/main/java/iuh/fit/se/HelloController.java
+++ src/main/java/iuh/fit/se/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Khoi tao du an laptopshop";
+        return "Khoi tao du an laptopshop, hello";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access";
     }
 
 }
