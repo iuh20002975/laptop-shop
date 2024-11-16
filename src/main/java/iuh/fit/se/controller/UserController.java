f0cd5683383a5db4d79f90c83a14cdbe007ec408
Nguyen Long <longsky0912624119@gmail.com>
2024-11-16 11:43:32 +0700

    add config view jsp

--- src/main/java/iuh/fit/se/controller/UserController.java
+++ src/main/java/iuh/fit/se/controller/UserController.java
@@ -9,30 +9,35 @@ import iuh.fit.se.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-
-//     }
-
-// }
-
-@RestController
+@Controller
 public class UserController {
-
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
 
     }
-
 }
+
+// @RestController
+// public class UserController {
+// DI: dependencies injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+
+// }
+
+// }
