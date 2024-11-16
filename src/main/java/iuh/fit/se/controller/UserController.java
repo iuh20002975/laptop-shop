1bc4b51a5f447c3a4b629cadba44b0ee200137fb
Nguyen Long <longsky0912624119@gmail.com>
2024-11-16 19:36:05 +0700

    add form create new user basic

--- src/main/java/iuh/fit/se/controller/UserController.java
+++ src/main/java/iuh/fit/se/controller/UserController.java
@@ -1,13 +1,17 @@
 package iuh.fit.se.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import iuh.fit.se.domain.User;
 import iuh.fit.se.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
@@ -18,26 +22,27 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("name1", test);
+        model.addAttribute("name2", "from controller with model");
         return "hello";
 
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+
+        model.addAttribute("newUser", new User());
+
+        return "admin/user/create";
+
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User name1) {
+        System.out.println("run here" + name1);
+        return "hello";
+    }
+
 }
-
-// @RestController
-// public class UserController {
-// DI: dependencies injection
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
-
-// }
-
-// }
