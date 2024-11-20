3da9472cc8f1f66ebcc417c661cc17509243899a
Nguyen Long <longsky0912624119@gmail.com>
2024-11-20 21:52:21 +0700

    add table, layout for project

---
+++ src/main/java/iuh/fit/se/controller/admin/ProductController.java
@@ -0,0 +1,14 @@
+package iuh.fit.se.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/admin/product")
+    public String getDashboard() {
+        return "admin/product/show";
+    }
+
+}
