493bfb92c3a985e4969f9d2206333718bf9d254a
Nguyen Long <longsky0912624119@gmail.com>
2024-11-20 13:59:09 +0700

    add anotation for models

--- src/main/java/iuh/fit/se/domain/Order.java
+++ src/main/java/iuh/fit/se/domain/Order.java
@@ -1,11 +1,18 @@
 package iuh.fit.se.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,6 +20,13 @@ public class Order {
     private double totalPrice;
 
     // userId
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order")
+    List<OrderDetail> orderDetails;
+
     public long getId() {
         return id;
     }
