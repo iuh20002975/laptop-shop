c1f6826d260401c71bc574df8ccfa40ebf6b24ff
Nguyen Long <longsky0912624119@gmail.com>
2024-11-26 16:31:20 +0700

    update service upload file

--- src/main/java/iuh/fit/se/controller/admin/UserController.java
+++ src/main/java/iuh/fit/se/controller/admin/UserController.java
@@ -1,15 +1,22 @@
 package iuh.fit.se.controller.admin;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import iuh.fit.se.domain.User;
 import iuh.fit.se.repository.UserRepository;
+import iuh.fit.se.service.UploadService;
 import iuh.fit.se.service.UserService;
+import jakarta.servlet.ServletContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,27 +28,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
-    private final UserService userService;
 
-    public UserController(UserService userService) {
+    private final UserService userService;
+    private final UploadService uploadService;
+
+    public UserController(
+            UploadService uploadService,
+            UserService userService, ServletContext servletContext) {
         this.userService = userService;
+        this.uploadService = uploadService;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        List<User> arrUsers = this.userService.getAllUsersByEmail("long1@gmail.com");
+        List<User> arrUsers = this.userService.getAllUsersByEmail("1@gmail.com");
         System.out.println(arrUsers);
 
-        model.addAttribute("name1", "test");
-        model.addAttribute("name2", "from controller with model");
+        model.addAttribute("eric", "test");
+        model.addAttribute("hoidanit", "from controller with model");
         return "hello";
-
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         List<User> users = this.userService.getAllUsers();
-        model.addAttribute("user1", users);
+        model.addAttribute("users1", users);
         return "admin/user/show";
     }
 
@@ -53,46 +64,46 @@ public class UserController {
         return "admin/user/detail";
     }
 
-    @RequestMapping("/admin/user/create") // GET
+    @GetMapping("/admin/user/create") // GET
     public String getCreateUserPage(Model model) {
-
         model.addAttribute("newUser", new User());
-
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User user1) {
+    @PostMapping(value = "/admin/user/create")
+    public String createUserPage(Model model,
+            @ModelAttribute("newUser") User hoidanit,
+            @RequestParam("hoidanitFile") MultipartFile file) {
 
-        this.userService.handleSaveUser(user1);
+        String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
+
+        // this.userService.handleSaveUser(hoidanit);
         return "redirect:/admin/user";
     }
 
     @RequestMapping("/admin/user/update/{id}") // GET
     public String getUpdateUserPage(Model model, @PathVariable long id) {
         User currentUser = this.userService.getUserById(id);
-
         model.addAttribute("newUser", currentUser);
-
         return "admin/user/update";
     }
 
     @PostMapping("/admin/user/update")
-    public String postUpdateUser(Model model, @ModelAttribute("newUser") User user1) {
-        User currentUser = this.userService.getUserById(user1.getId());
+    public String postUpdateUser(Model model, @ModelAttribute("newUser") User hoidanit) {
+        User currentUser = this.userService.getUserById(hoidanit.getId());
         if (currentUser != null) {
-            currentUser.setAddress(user1.getAddress());
-            currentUser.setFullName(user1.getFullName());
-            currentUser.setPhone(user1.getPhone());
+            currentUser.setAddress(hoidanit.getAddress());
+            currentUser.setFullName(hoidanit.getFullName());
+            currentUser.setPhone(hoidanit.getPhone());
 
+            // bug here
             this.userService.handleSaveUser(currentUser);
-
         }
         return "redirect:/admin/user";
     }
 
     @GetMapping("/admin/user/delete/{id}")
-    public String getDeleterUserPage(Model model, @PathVariable long id) {
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
         model.addAttribute("id", id);
         // User user = new User();
         // user.setId(id);
@@ -101,9 +112,8 @@ public class UserController {
     }
 
     @PostMapping("/admin/user/delete")
-    public String postDeleteUserPage(Model model, @ModelAttribute("newUser") User user1) {
-        this.userService.deleteAUser(user1.getId());
+    public String postDeleteUser(Model model, @ModelAttribute("newUser") User eric) {
+        this.userService.deleteAUser(eric.getId());
         return "redirect:/admin/user";
     }
-
 }
