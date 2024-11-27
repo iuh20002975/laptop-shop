8296f305b4f592109517943eeb50e39bfb60c9de
Nguyen Long <longsky0912624119@gmail.com>
2024-11-28 02:10:47 +0700

    format price vnd, update view detail product

--- src/main/java/iuh/fit/se/controller/client/ItemController.java
+++ src/main/java/iuh/fit/se/controller/client/ItemController.java
@@ -5,12 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import iuh.fit.se.domain.Product;
+import iuh.fit.se.service.ProductService;
+
 @Controller
 public class ItemController {
 
-    @GetMapping("/product/{id}")
-    public String getProductPage(Model model, @PathVariable long id) {
-        return "client/product/detail";
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
     }
 
+    @GetMapping("/product/{id}")
+    public String getProductPage(Model model, @PathVariable long id) {
+        Product pr = this.productService.fetchProductById(id).get();
+        model.addAttribute("product", pr);
+        model.addAttribute("id", id);
+        return "client/product/detail";
+    }
 }
