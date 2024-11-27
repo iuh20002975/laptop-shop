0acf2922b009b332d8fa5dbb1e1bc40aeb62e887
Nguyen Long <longsky0912624119@gmail.com>
2024-11-27 19:11:09 +0700

    add page create new product for admin

--- src/main/java/iuh/fit/se/controller/admin/ProductController.java
+++ src/main/java/iuh/fit/se/controller/admin/ProductController.java
@@ -1,14 +1,24 @@
 package iuh.fit.se.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import iuh.fit.se.domain.Product;
 
 @Controller
 public class ProductController {
 
     @GetMapping("/admin/product")
-    public String getDashboard() {
+    public String getProduct() {
         return "admin/product/show";
+    }
+
+    @GetMapping("/admin/product/create")
+    public String getCreateProductPage(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
     }
 
 }
