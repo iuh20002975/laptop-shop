78c52e831569f5e2c0762b3d47a4d5c9fa2372ec
Nguyen Long <longsky0912624119@gmail.com>
2024-11-29 23:47:29 +0700

    add logic add to cart v2, add detail cart page

--- src/main/java/iuh/fit/se/repository/CartDetailRepository.java
+++ src/main/java/iuh/fit/se/repository/CartDetailRepository.java
@@ -3,9 +3,13 @@ package iuh.fit.se.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import iuh.fit.se.domain.Cart;
 import iuh.fit.se.domain.CartDetail;
+import iuh.fit.se.domain.Product;
 
 @Repository
 public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
+    boolean existsByCartAndProduct(Cart cart, Product product);
 
+    CartDetail findByCartAndProduct(Cart cart, Product product);
 }
