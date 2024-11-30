b1dd4a9aea64a4be258d4dcb0240d9752ed90c78
Nguyen Long <longsky0912624119@gmail.com>
2024-11-30 10:25:14 +0700

    update dashboard, statistic user, product, order

--- src/main/java/iuh/fit/se/service/UserService.java
+++ src/main/java/iuh/fit/se/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import iuh.fit.se.domain.Role;
 import iuh.fit.se.domain.User;
 import iuh.fit.se.domain.dto.RegisterDTO;
+import iuh.fit.se.repository.OrderRepository;
+import iuh.fit.se.repository.ProductRepository;
 import iuh.fit.se.repository.RoleRepository;
 import iuh.fit.se.repository.UserRepository;
 
@@ -14,11 +16,17 @@ import iuh.fit.se.repository.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
     public UserService(UserRepository userRepository,
-            RoleRepository roleRepository) {
+            RoleRepository roleRepository,
+            ProductRepository productRepository,
+            OrderRepository orderRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
     }
 
     public List<User> getAllUsers() {
@@ -61,5 +69,17 @@ public class UserService {
 
     public User getUserByEmail(String email) {
         return this.userRepository.findByEmail(email);
+    }
+
+    public long countUsers() {
+        return this.userRepository.count();
+    }
+
+    public long countProducts() {
+        return this.productRepository.count();
+    }
+
+    public long countOrders() {
+        return this.orderRepository.count();
     }
 }
